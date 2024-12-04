4782960d311f84a1190bb4e1c2efb35bd81f1fee
timothy <timothyignatiusa@gmail.com>
2024-12-04 04:28:56 +0100

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import { connectDB } from './config/database.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database.js";
 
-import authRouter from './routes/auth.js';
-import taskRouter from './routes/task.js';
+import authRouter from "./routes/auth.js";
+import taskRouter from "./routes/task.js";
 
 const app = express();
 dotenv.config();
@@ -16,20 +16,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://task-manager-app-three-woad.vercel.app',
+    origin: "https://task-manager-app-three-woad.vercel.app/",
     credentials: true,
   })
 );
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRouter);
-app.use('/api/tasks', taskRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/tasks", taskRouter);
 
 // errorHandler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server error';
+  const message = err.message || "Internal server error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
